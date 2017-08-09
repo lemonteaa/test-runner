03b41b7fec84e124daffabd81e7776a99a938da4
lemontea <lemontea.Tom@gmail.com>
2017-08-09 21:46:01 +0800

    Add UI (I)

--- src/lt/plugins/test_runner.cljs
+++ src/lt/plugins/test_runner.cljs
@@ -7,17 +7,28 @@
             [lt.plugins.clojure.nrepl :as nrepl])
   (:require-macros [lt.macros :refer [defui behavior]]))
 
-(defui midje-tester-panel [this]
+(defui midje-tester-panel [this proj-name]
   [:div
-    [:h1 "Hello from test_runner more test"]
-    [:h2 (:path this)]])
+    (config-panel this proj-name)])
+
+(defui config-panel [this proj-name]
+  [:div
+    [:h2 (str "Project: " proj-name)]
+    [:div
+      "Testing Library: "
+      [:select {:name "test-lib"}
+        [:option {:value :clojure.test } "clojure.test"]
+        [:option {:value :midje } "midje"]]
+      [:input {:type "checkbox" :id "auto-test"}]
+      [:label {:for "auto-test"} "Auto-test On Save?"]
+      [:button "Run!"]]])
 
 (object/object* ::test_runner.midje-tester
                 :tags [:test_runner.midje-tester]
                 :behaviors [::on-close-destroy]
                 :name "Midje Tester"
-                :init (fn [this]
-                        (midje-tester-panel this)))
+                :init (fn [this proj-name]
+                        (midje-tester-panel this proj-name)))
 
 (behavior ::on-close-destroy
           :triggers #{:close}
@@ -51,7 +62,7 @@
           :triggers #{:menu-selected}
           :reaction (fn [this path]
                       (object/update! midje-tester [:path] (constantly path))
-                      (object/update! midje-tester [:content] (constantly (midje-tester-panel (deref midje-tester))))
+                      (object/update! midje-tester [:content] (constantly (midje-tester-panel (deref midje-tester) "My project")))
                       (prn (deref midje-tester))
                       (tabs/add-or-focus! midje-tester)))
 
@@ -68,14 +79,14 @@
 (object/add-behavior! (-> @(nth (object/by-tag :editor) 3) :client :default) ::on-recv-response)
 
 
-(def midje-tester (object/create ::test_runner.midje-tester))
+(def midje-tester (object/create ::test_runner.midje-tester "My project"))
 
 (cmd/command {:command ::say-hello
               :desc "test_runner: Say Hello world"
               :exec (fn []
                       (prn "Test")
                       (object/update! midje-tester [:path] (constantly (-> @(lt.objs.editor.pool/last-active) :info :path)))
-                      (object/update! midje-tester [:content] (constantly (midje-tester-panel (deref midje-tester))))
+                      (object/update! midje-tester [:content] (constantly (midje-tester-panel (deref midje-tester) "My project")))
                       (tabs/add-or-focus! midje-tester))})
 
 workspace/root
